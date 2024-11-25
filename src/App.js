290df3fcbea27021b5798174c14e7c2ae6712e96
VenkatreddyPadala <pvr2004@gmail.com>
2024-11-25 23:35:54 +0530

    Worked on text area

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
 function App() {
+  const [mode, setMode] = useState('light'); 
+  const [alert, setAlert] = useState(null);
+  const showAlert =(message,type)=>{
+    setAlert({
+      msg:message,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#192734';
+      document.body.style.color = 'white'; 
+      showAlert("dark Mode enabled","success");
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'; 
+      document.body.style.color = 'black';
+      showAlert("light Mode enabled","primary");
+    }
+  };
+
   return (
     <>
-     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Text Utils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <Navbar title="TextUtils" aboutTitle="About us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode}/>
+      </div>
+      
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+export default function TextForm(props) {
+    const handleUpClick = (event) => {
+        console.log("Upper Case Clicked");
+        if(text.length ===0){
+            props.showAlert("Text is empty. Please enter some text.","danger");
+            return;
+        }
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Text is converted into Uppercase","success");
+    };
+    const handleLoClick = (event) => {
+        console.log("Lower Case Clicked");
+        if(text.length ===0){
+            props.showAlert("Text is empty. Please enter some text.","danger");
+            return;
+        }
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Text is converted into Lowercase","primary");
+    };
+    const handleOnChange = (event) => {
+        console.log("On Change");
+        setText(event.target.value);
+    };
+    const handleClearClick = (event) => {
+        console.log("Clear Clicked");
+        if(text.length ===0){
+            props.showAlert("There is nothing to clear Please type somthing in the Text area","danger");
+            return;
+        }
+        setText("");
+        props.showAlert("Text is cleared","danger");
+    };
+    const handleCopyClick = (event) => {
+        console.log("Copy Clicked");
+        if(text.length ===0){
+            props.showAlert("There is nothing to copy please type something","danger");
+            return;
+        }
+        navigator.clipboard.writeText(text);
+        props.showAlert("Text has been copied to clipboard","warning");
+    };
+    const handleFindEmailClick = () => {
+        console.log("Email Finder Clicked");
+        let emailRegex = /\S+@\S+\.\S+/;
+        let emails = text.match(emailRegex);
+        if (emails) {
+            setText("Email addresses found: " + emails.join(", "));
+        } else {
+            props.showAlert("No email address Found","danger");
+        }
+    };
+    const [text, setText] = useState("");
+    console.log(useState("Enter text here"));
+    return (
+        <>
+            <div className='container my-3'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder='Enter text here' style={{
+                        backgroundColor: props.mode === "dark" ? '#B2BEB5' : "white",
+                        color: props.mode === "dark" ? 'white' : "#333",
+                        '::placeholder': { color: props.mode === 'dark' ? '#aaa' : '#666' }
+                    }}></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className="btn btn-danger mx-1" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-info mx-1" onClick={handleCopyClick}>Copy Text</button>
+                <button className="btn btn-info mx-1" onClick={handleFindEmailClick}>Email Finder</button>
+            </div>
+            <div className="container my-5">
+                <h2>Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter something in the text box to show the preview here"}</p>
+            </div>
+        </>
+    )
+}
